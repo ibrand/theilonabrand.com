d357e3f7fdb398193624623c2d2672c304b7740e
Ilona Brand <theilonabrand@gmail.com>
2020-01-29 13:30:22 -0500

    Make the favicon mine

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -7,8 +7,6 @@ import MobileHeader from "../components/Header/MobileHeader"
 import Footer from '../components/Footer/Footer'
 
 import appleTouchIcon from '../../static/assets/favicon/apple-touch-icon.png'
-import favicon32 from '../../static/assets/favicon/favicon-32x32.png'
-import favicon16 from '../../static/assets/favicon/favicon-16x16.png'
 
 const BaseLayout = ({ children }) => (
   <div>
@@ -17,8 +15,6 @@ const BaseLayout = ({ children }) => (
       <meta name="description" content={ config.description } />
       <meta name="keywords" content={ config.keywords } />
       <link rel="apple-touch-icon" sizes="180x180" href={ appleTouchIcon } />
-      <link rel="icon" type="image/png" sizes="32x32" href={ favicon32 } />
-      <link rel="icon" type="image/png" sizes="16x16" href={ favicon16 } />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
     </Helmet>
