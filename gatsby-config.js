c36896d1b00b17a32d1cb64dcc4a32da90c59d9d
Ilona Brand <theilonabrand@gmail.com>
2020-01-30 12:54:13 -0500

    gatsby manifest fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,11 +50,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "Ryosuke - Designer, Developer, & Influencer",
-        short_name: "Ryosuke",
+        name: "Ilona Brand's Homepage",
+        short_name: "Ilona Brand",
         start_url: "/",
         background_color: "#F5F5F5",
-        theme_color: "#005CDD",
+        theme_color: "#000000",
         display: "minimal-ui",
         icon: `static/assets/favicon/android-chrome-512x512.png`, // This path is relative to the root of the site.
         // icons: [
